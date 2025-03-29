4c5ad9a0308f26f8f40b2a7d5169d7440b6278d7
kineret-cohen <kineret.cohen972@gmail.com>
2025-03-29 21:36:30 +0300

    apply setup for cart access

--- plugins/woocommerce-exquisiterugs/public/class-woocommerce-exquisiterugs-public.php
+++ plugins/woocommerce-exquisiterugs/public/class-woocommerce-exquisiterugs-public.php
@@ -52,7 +52,100 @@ class WC_ExquisiteRugs_Public {
 		$this->plugin_name = $plugin_name;
 		$this->version = $version;
 
+        // Add cart access control
+        add_action('woocommerce_init', array($this, 'disable_cart_for_user_role'));
+        add_filter('wp_nav_menu_objects', array($this, 'hide_cart_icon_for_user_role'), 10, 2);
 	}
+
+    /**
+     * Check if current user can use cart
+     *
+     * @since    1.0.0
+     * @return   boolean
+     */
+    private function can_use_cart() {
+        // Site admins always have access
+        if (is_site_admin()) {
+            return true;
+        }
+
+        $cart_access = get_option('wc_exquisiterugs_cart_access', 'disable');
+        
+        switch ($cart_access) {
+            case 'all':
+                return true;
+            case 'selected':
+                $allowed_users = get_option('wc_exquisiterugs_allowed_users', '');
+                if (empty($allowed_users)) {
+                    return false;
+                }
+                $current_user = wp_get_current_user();
+                $allowed_users_array = array_map('trim', explode(',', $allowed_users));
+                return in_array($current_user->user_login, $allowed_users_array);
+            case 'disable':
+            default:
+                return false;
+        }
+    }
+
+    /**
+     * Disable cart functionality for restricted users
+     *
+     * @since    1.0.0
+     */
+    public function disable_cart_for_user_role() {
+        if (!$this->can_use_cart()) {
+            // Remove cart functionality
+            remove_action('woocommerce_before_cart', 'woocommerce_cart_redirect_if_empty', 10);
+            remove_action('woocommerce_before_checkout_form', 'woocommerce_checkout_coupon_form', 10);
+            remove_action('woocommerce_cart_update_totals', 'woocommerce_cart_update_totals', 10);
+            remove_action('woocommerce_after_cart', 'woocommerce_cart_collaterals', 10);
+            remove_action('woocommerce_cart_contents', 'woocommerce_cart_contents', 10);
+            
+            remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10);
+            remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30);
+            remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_price', 10);
+            
+            add_action('woocommerce_before_cart', array($this, 'cart_disabled_message'));
+        }
+    }
+
+    /**
+     * Hide cart icon from menu for restricted users
+     *
+     * @since    1.0.0
+     * @param    array    $menu_objects    Menu items
+     * @param    object   $args            Menu arguments
+     * @return   array    Modified menu items
+     */
+    public function hide_cart_icon_for_user_role($menu_objects, $args) {
+        if (!str_contains($args->theme_location, 'navigation') || $this->can_use_cart()) {
+            return $menu_objects;
+        }
+        
+        $menu_items = array(
+            'Cart',
+            'My Account'
+        );
+        
+        foreach ($menu_objects as $key => $menu_object) {
+            if (!in_array($menu_object->title, $menu_items)) {
+                continue;
+            }
+            unset($menu_objects[$key]);
+        }
+            
+        return $menu_objects;
+    }
+
+    /**
+     * Display cart disabled message
+     *
+     * @since    1.0.0
+     */
+    public function cart_disabled_message() {
+        echo '<div class="woocommerce-info">Your shopping cart is disabled. Please contact support for assistance.</div>';
+    }
 
 	/**
 	 * Register the stylesheets for the public-facing side of the site.
